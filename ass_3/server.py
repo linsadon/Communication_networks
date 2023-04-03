b1fc491b5d302624a2d00601895480b66d7a60d4
yuval <yuvalamar15@gmail.com>
2023-04-04 00:48:27 +0300

    avoid from added duplicate addr

--- ass_3/server.py
+++ ass_3/server.py
@@ -7,6 +7,21 @@ import json
 serversdict=[]
 clientsdict={}
 
+
+def pingallusers():
+    socket_objects = []
+    global serversdict
+    for address in serversdict:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((my_ip, my_port))
+        sock.connect(address)
+        socket_objects.append(sock)
+    # for sock in socket_objects:
+    #     sock.send("conn try".encode())
+    for sock in socket_objects:
+        sock.close()
+
 def senddict(conn):
     conn.sendall(pickle.dumps(serversdict))
 
@@ -14,10 +29,13 @@ def acceptconnection(sock):
     while True:
         conn, client_address = sock.accept()
         print('new connection from', client_address)
-        senddict(conn)
         global serversdict
-        serversdict.append(conn.getpeername())
-        threading.Thread(target=respond_to_client, args=(conn, client_address)).start()
+        if client_address not in serversdict:
+            senddict(conn)
+            serversdict.append(client_address)
+        conn.close()
+        print("my list : " + str(serversdict))
+        #threading.Thread(target=respond_to_client, args=(conn, client_address)).start()
 
 
 def respond_to_client(conn_socket, client_address):
@@ -43,13 +61,16 @@ for i in ports:
             sock.connect((my_ip, i))
             # sock.send(str(input("enter your name : ")).encode())
             ###################################################
+            #0-----1-----2----------4----------6
+            #|type | sub |    len   |   sub    |
+            #|     |type |          |   len    |
             data = sock.recv(4096)
             picklearray = pickle.loads(data)
-            print(picklearray)
             ###################################################
             #print('received from ', sock.getpeername(), sock.recv(1024).decode())
             serversdict.append(sock.getpeername())
-            print(serversdict)
+            serversdict.extend(picklearray)
+            print("origin : " +str(serversdict))
             sock.close()
             break
     except ConnectionRefusedError:
@@ -57,4 +78,5 @@ for i in ports:
     except socket.error as e:
         print(f"Error connecting to server on port {i}: {e}")
 print("End of searching")
+pingallusers()
 
