ee632ed31bb47445d9fe0103fbd1b7db64e3d124
yuval <yuvalamar15@gmail.com>
2023-04-03 20:50:26 +0300

    sending array of addresses

--- ass_3/server.py
+++ ass_3/server.py
@@ -1,16 +1,22 @@
 import socket
 import threading
+import pickle
+import json
+
 
 serversdict=[]
 clientsdict={}
 
+def senddict(conn):
+    conn.sendall(pickle.dumps(serversdict))
 
 def acceptconnection(sock):
     while True:
         conn, client_address = sock.accept()
         print('new connection from', client_address)
+        senddict(conn)
         global serversdict
-        serversdict.append(conn)
+        serversdict.append(conn.getpeername())
         threading.Thread(target=respond_to_client, args=(conn, client_address)).start()
 
 
@@ -34,13 +40,17 @@ for i in ports:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             sock.bind((my_ip, my_port))
-            a = sock.connect((my_ip, i))
-
+            sock.connect((my_ip, i))
             # sock.send(str(input("enter your name : ")).encode())
-            print('received from ', sock.getpeername(), sock.recv(1024).decode())
-            print(sock.getsockopt(socket.SOL_SOCKET, socket.SO_TYPE))
+            ###################################################
+            data = sock.recv(4096)
+            picklearray = pickle.loads(data)
+            print(picklearray)
+            ###################################################
+            #print('received from ', sock.getpeername(), sock.recv(1024).decode())
+            serversdict.append(sock.getpeername())
+            print(serversdict)
             sock.close()
-            print('ddddd')
             break
     except ConnectionRefusedError:
         print(f"The server is not active at port {i}")
